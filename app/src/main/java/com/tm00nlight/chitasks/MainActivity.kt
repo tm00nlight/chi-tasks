9fb350198b65ffa39dc9cf5206ac627c804aa397
tm00nlight <95237741+tm00nlight@users.noreply.github.com>
2022-08-29 23:03:47 +0300

    Middle - done

--- app/src/main/java/com/tm00nlight/chitasks/MainActivity.kt
+++ app/src/main/java/com/tm00nlight/chitasks/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 
+private const val KEY_COUNTER = "ClickCounter"
+
 class MainActivity : AppCompatActivity() {
     var counter: Int = 0
 
@@ -15,9 +17,18 @@ class MainActivity : AppCompatActivity() {
         val textView = findViewById<TextView>(R.id.textView)
         val button = findViewById<Button>(R.id.button)
 
+        counter = savedInstanceState?.getInt(KEY_COUNTER, 0)?:0
+        textView.text = getString(R.string.text_clicked, counter)
+
         button.setOnClickListener {
             counter++
             textView.text = getString(R.string.text_clicked, counter)
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putInt(KEY_COUNTER, counter)
     }
 }
