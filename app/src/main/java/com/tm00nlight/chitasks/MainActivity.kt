6881e0f97b7272125ff0741c4eb69eb276c31c55
tm00nlight <95237741+tm00nlight@users.noreply.github.com>
2022-08-29 21:59:52 +0300

    Junior - done

--- app/src/main/java/com/tm00nlight/chitasks/MainActivity.kt
+++ app/src/main/java/com/tm00nlight/chitasks/MainActivity.kt
@@ -2,10 +2,22 @@ package com.tm00nlight.chitasks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    var counter: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.textView)
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            counter++
+            textView.text = getString(R.string.text_clicked, counter)
+        }
     }
 }
